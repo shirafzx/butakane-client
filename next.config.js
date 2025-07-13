14365831075c055742ffdb75c4aab3d320113c03
artnont <artnont34256@gmail.com>
2025-07-13 13:34:38 +0700

    fix loopback 308

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: false, // or true, but be consistent with Traefik rewrite rules
+    trustHost: true,
+};
 
 module.exports = nextConfig;
